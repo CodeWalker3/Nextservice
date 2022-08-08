c5951bbfd95773f3ec240ab6713f3bd7b15ee31e
Gustavo <87251711+CodeWalker3@users.noreply.github.com>
2022-08-08 16:06:06 -0300

    Add files via upload

---
+++ pages/cardapio.js
@@ -0,0 +1,116 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/cardapio.module.css'
+import Script from 'next/script'
+
+
+export default function Cardapio(){
+    return (
+        <>        
+<section className={styles.home} id="home">
+
+<div className={styles.content}>
+    <h3>Cardápio</h3>
+</div>
+
+</section>
+
+
+<section className={styles.categories} id="categories">
+<a href="#" className="btn">Pequena</a>
+<a href="#" className="btn">Média</a>
+<a href="#" className="btn">Grande</a>
+
+
+<div className="box-container">
+
+    <div className="box">
+        <img src="image/pizza.jpg" alt=""/>
+        <h3>2 QUEIJOS</h3>
+        <p>Molho de Tomate, Mussarela, Catupiry, Orégano, Azeitona
+        </p>
+        <p>R$ 30,00 </p>
+        <a href="forma-pagamento.html" className="btn">Comprar</a>
+    </div>
+
+    <div className="box">
+        <img src="image/pizza.jpg" alt=""/>
+        <h3>2 QUEIJOS</h3>
+        <p>Molho de Tomate, Mussarela, Catupiry, Orégano, Azeitona
+        </p>
+        <p>R$ 30,00 </p>
+        <a href="forma-pagamento.html" className="btn">Comprar</a>
+    </div>
+
+    <div className="box">
+        <img src="image/pizza.jpg" alt=""/>
+        <h3>2 QUEIJOS</h3>
+        <p>Molho de Tomate, Mussarela, Catupiry, Orégano, Azeitona
+        </p>
+        <p>R$ 30,00 </p>
+        <a href="forma-pagamento.html" className="btn">Comprar</a>
+    </div>
+
+    <div className="box">
+        <img src="image/pizza.jpg" alt=""/>
+        <h3>2 QUEIJOS</h3>
+        <p>Molho de Tomate, Mussarela, Catupiry, Orégano, Azeitona
+        </p>
+        <p>R$ 30,00 </p>
+        <a href="forma-pagamento.html" className="btn">Comprar</a>
+    </div>
+    <div className="box">
+        <img src="image/pizza.jpg" alt=""/>
+        <h3>2 QUEIJOS</h3>
+        <p>Molho de Tomate, Mussarela, Catupiry, Orégano, Azeitona
+        </p>
+        <p>R$ 30,00 </p>
+        <a href="forma-pagamento.html" className="btn">Comprar</a>
+    </div>
+
+    <div className="box">
+        <img src="image/pizza.jpg" alt=""/>
+        <h3>2 QUEIJOS</h3>
+        <p>Molho de Tomate, Mussarela, Catupiry, Orégano, Azeitona
+        </p>
+        <p>R$ 30,00 </p>
+        <a href="forma-pagamento.html" className="btn">Comprar</a>
+    </div>
+
+    <div className="box">
+        <img src="image/pizza.jpg" alt=""/>
+        <h3>2 QUEIJOS</h3>
+        <p>Molho de Tomate, Mussarela, Catupiry, Orégano, Azeitona
+        </p>
+        <p>R$ 30,00 </p>
+        <a href="forma-pagamento.html" className="btn">Comprar</a>
+    </div>
+
+    <div className="box">
+        <img src="image/pizza.jpg" alt=""/>
+        <h3>2 QUEIJOS</h3>
+        <p>Molho de Tomate, Mussarela, Catupiry, Orégano, Azeitona
+        </p>
+        <p>R$ 30,00 </p>
+
+        <a href="forma-pagamento.html" className="btn">Comprar</a>
+    </div>
+
+
+</div>
+
+</section>
+
+
+
+
+
+<Script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></Script>
+<Script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossOrigin="anonymous"></Script>
+<Script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></Script>
+<Script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></Script>
+
+<Script src="js/script.js"></Script>
+        </>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,172 @@
+import Head from 'next/head'
+
+
+
+
+// pages/index.tsx
+
+export default function Home() {
+  return (
+    <>
+
+<Head>
+{/* <meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>On Air Pizza</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+<link rel="shortcut icon" href="image/logo.png" />
+<!-- font awesome cdn link  -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+<!-- custom css file link  -->
+<link rel="stylesheet" href="css/style.css"> */}
+
+</Head>
+
+
+
+
+
+<body>
+
+=
+
+    <section className="home" id="home">
+
+        <div className="content">
+            <h3>A melhor pizzaria da região</h3>{/* 
+            <a href="cardapio.html" className="btn">Peça Agora</a> */}
+        </div>
+
+    </section>
+
+
+    <section className="promocoes container-fluid ">
+        <h3 className="heading">Promoções</h3>
+
+        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+            <ol className="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img className="d-block w-100" src="image/promo1.jpg" alt="First slide" width="650" height="550"/>
+                </div>
+                <div className="carousel-item">
+                    <img className="d-block w-100" src="image/promo2.jpg" alt="Second slide" width="650" height="550"/>
+                </div>
+                <div className="carousel-item">
+                    <img className="d-block w-100" src="image/promo3.jpg" alt="Third slide" width="650" height="550"/>
+                </div>
+            </div>
+{/*             <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
+            </a>
+ */}        </div>
+    </section>
+
+    <section className="promocoes container-fluid ">
+        <h3 className="heading">Combos</h3>
+
+        <div id="carouselSite" className="carousel slide" data-ride="carousel">
+            <ol className="carousel-indicators">
+                <li data-target="#carouselSite" data-slide-to="0" className="active"></li>
+                <li data-target="#carouselSite" data-slide-to="1"></li>
+                <li data-target="#carouselSite" data-slide-to="2"></li>
+            </ol>
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img className="d-block w-100" src="image/COMBO1.png" alt="First slide" width="650" height="550"/>
+                </div>
+                <div className="carousel-item">
+                    <img className="d-block w-100" src="image/combo2.jpg" alt="Second slide" width="650" height="550"/>
+                </div>
+                <div className="carousel-item">
+                    <img className="d-block w-100" src="image/combo3.jpg" alt="Third slide" width="650" height="550"/>
+                </div>
+            </div>
+{/*             <a className="carousel-control-prev" href="#carouselSite" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#carouselSite" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
+            </a>
+ */}        </div>
+    </section>
+{/* 
+    <!-- home section ends -->
+
+    <!-- features section starts  --> */}
+
+    <section className="features" id="features">
+
+        <h1 className="heading"> Nosso<span>Serviço</span> </h1>
+
+        <div className="box-container">
+
+            <div className="box">
+                <img src="image/feature-img-1.png" alt=""/>
+                <h3>Alimentos frescos e Orgânicos</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, earum!</p>
+
+            </div>
+
+            <div className="box">
+                <img src="image/feature-img-2.png" alt=""/>
+                <h3>Delivery grátis</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, earum!</p>
+
+            </div>
+
+            <div className="box">
+                <img src="image/feature-img-3.png" alt=""/>
+                <h3>Pagamento Fácil</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, earum!</p>
+
+            </div>
+
+        </div>
+
+    </section>
+
+
+
+   {/*  <!-- footer section ends --> */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/*     <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <!-- custom js file link  -->
+    <script src="js/script.js"></script>
+ */}
+</body>
+
+</>
+  )
+}
